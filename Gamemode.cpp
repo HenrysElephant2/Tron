3db1bedca01eb9ca138786c9d3a562a20f18f938
HenrysElephant1 <jacu9594@colorado.edu>
2018-11-15 13:40:07 -0700

    Adding Makefile

--- Gamemode.cpp
+++
@@ -1,6 +0,0 @@
-class Gamemode{
-	void key(/* TODO*/) = 0; // need to format this specifically for SDL instead of GLUT
-	void special(int key, int x, int y) = 0; /*NEEDED in GLUT, maybe different in other system */
-	void mouse(/*TODO*/) = 0;
-	void display() = 0;
-}
